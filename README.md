0b3f8fca26cbec7d7ec0ad3e7cd0be34ec66ffd4
matheusanmo <matheusanmo@gmail.com>
2022-10-09 11:04:55 -0300

    pre nuke positivo

--- README.md
+++ README.md
@@ -5,15 +5,29 @@ usa api discog para encontrar labels releases artistas interligados
 TODO: usagem cmd
 
 ## tecnologia
+### python
+py3
+
+### pipenv
 o projeto usa pipenv para gerir dependencias e versao py
+
+### bibliotecas padores
 python usando apenas bibliotecas padroes: 
     - socket: para sockets
+    - dataclass
     - cmd: para interface cli
     - unittest: para testes unitarios
     - logging: logar
+    - json:
 
+#### uso da biblioteca socket
+
+### bibliotecas externas para desenvolvimentos
 dependencias externas, usadas apenas para dev:
     - pylint: linter
     - flake8: linter
     - black: code formatter
     - ipython: repl
+
+## autoria
+https://github.com/matheusanmo outubro 2022
--- main.py
+++ main.py
@@ -1,7 +1,34 @@
-def main():
-    pass
+import socket
 
-if __name__ == "__main__":
-    main()
+HOST = 'example.com'    # The remote host
+PORT = 80              # The same port as used by the server
+http_msg = (b"GET / HTTP/1.1\n"
+            b"Host: example.com\n"
+            b"User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:105.0) Gecko/20100101 Firefox/105.0\n"
+            b"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8\n"
+            b"Accept-Language: en-US,en;q=0.5\n"
+            b"Accept-Encoding: gzip, deflate\n"
+            b"Connection: keep-alive\n"
+            b"Upgrade-Insecure-Requests: 1\n"
+            b"If-Modified-Since: Thu, 17 Oct 2019 07:18:26 GMT\n")
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(http_msg)
+    data = s.recv(2048)
+print('Received', repr(data))
+
+
+##def main():
+#    HOST = 'tcpbin.com'    # The remote host
+#    PORT = 4242              # The same port as used by the server
+#    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#        s.connect((HOST, PORT))
+#        s.sendall(b'Hello, world')
+#        data = s.recv(1024)
+#    print('Received', repr(data))
+#    pass
+#
+#if __name__ == "__main__":
+#    main()
 
 
